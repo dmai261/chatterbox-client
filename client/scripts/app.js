f5f858d84d5b96a4a9b2ccfe87152e9df75df154
dmai261 <dxmai1314@gmail.com>
2018-08-11 16:35:16 -0700

    Implemented room selector

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,15 +1,16 @@
 class App {
   constructor(username, message, room, server) {
     this.username = JSON.stringify(window.location.search.slice(10)).replace(/[^A-Za-z]/g, " ");
-    this.room = 'R00M';
+    this.room = [];
     this.server = 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages';
   }
   init() {
     let THIS = this;
 
     $(document).ready(function () {
-      $('body').on('click', '.username',() => {
-        THIS.handleUsernameClick();
+      $('body').on('click', '#username', (event) => {
+        THIS.handleUsernameClick($(event.target).text().toString());
+        // THIS.handleUsernameClick($('.username').);
       });
       
       $('.submit').on('click', () => {
@@ -36,7 +37,6 @@ class App {
           contentType: 'application/json',
           success: (data) => {
             $.each(data.results, (index, data) => {
-              // console.log(data);
               if (data.roomname === $('#roomSelect').val()) {
                 THIS.renderUsername(_.escape(data.username));
                 THIS.renderMessage(_.escape(data.text));
@@ -52,7 +52,6 @@ class App {
   }
 
   send(message) {
-    // console.log("insideSend", message);
     $.ajax({
       url: this.server,
       type: 'POST',
@@ -70,56 +69,65 @@ class App {
   
   fetch() {
     var roomList = [];
+    var users = {};
+    var THIS = this;
     $.ajax({
       url: this.server,
       type: 'GET',
       data: 'order=-createdAt',
       contentType: 'application/json',
       success: (data) => {
-        $.each(data.results, (index, data) => {
-          // console.log(data);
-          console.log(data);
-          roomList.push(data);
-          
-          // this.renderRoom(_.escape(data.roomname));
-          this.renderUsername(_.escape(data.username));
-          this.renderMessage(_.escape(data.text));
+        $.each(data.results, (index, data = _.escape(data)) => {
+          roomList.push(data.roomname);
+          if (!users[data.username]) {
+            users[data.username] = [];
+          } else {
+            users[data.username].push(data.text);
+          }
         });
+
+        _.uniq(roomList).forEach(function(element) {
+          if (element) {
+            THIS.renderRoom(element);
+          }
+        });
+        console.log(users);
+
+        for (var user in users) {
+          console.log(user);
+          users[user].forEach(function(person) { 
+            THIS.renderUsername(user);
+            THIS.renderMessage(user, person);
+          });
+        }
       },
       error: function (data) {
         console.error('chatterbox: Failed to send message', data);
       }
     });
-    console.log(roomList);
-    var uniqRooms = _.uniq(roomList);
-    console.log("uniqRooms",uniqRooms)
-    uniqRooms.forEach(function(element) {
-      THIS.renderRoom(element);
-    });
-    
   }
   
   clearMessages() {
     $('#chats').empty();
   }
+
   renderUsername(user) {
-    $('#chats').append(`<div class="username"> ${user}
-    </div>`);
+    $('#chats').append(`<div  id="username" class=${user}> ${user} </div>`);
   }
-  renderMessage(message) { 
-    $('#chats').append(`<div id="message">Message: ${message}
-    </div>`);
+  renderMessage(user, message) { 
+    $('#chats').append(`<div id="message" class=${user}>Message: ${message} </div>`);
   }
 
   renderRoom(room) {
-    
     $('#roomSelect').append(`<option>${room}</option>`);
   }
 
-  handleUsernameClick() {
-    console.log('wtf');
+  handleUsernameClick(username) {
+    username = username.trim();
+    // console.log(username);
+    $(`.${username}`).css('font-weight', 'bold');
   }
-
+  
   handleSubmit(message) {
     this.send(message);
   }
@@ -128,100 +136,3 @@ class App {
 let app = new App();
 app.init();
 app.fetch();
-
-
-// class App {
-//   constructor(username, message, room, server) {
-//     this.username = JSON.stringify(window.location.search.slice(10)).replace(/[^A-Za-z]/g, " ");
-//     this.room = 'R00M';
-//     this.server = 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages';
-//   }
-//   init() {
-//     let THIS = this;
-
-//     $(document).ready(function () {
-//       $('.username').on('click', () => {
-//         THIS.handleUsernameClick();
-//       });
-      
-//       $('.submit').on('click', () => {
-//         let newObj = {};
-//         newObj.username = THIS.username;
-//         newObj.text = $('#post').val();
-//         newObj.roomname = THIS.room;
-        
-//         THIS.handleSubmit(newObj);
-//       });
-      
-//       $('#refreshButton').on('click', () => {
-//         window.location.reload();
-//         THIS.fetch();
-//       });
-//     });
-//   }
-
-//   send(message) {
-//     console.log("insideSend", message);
-//     $.ajax({
-//       url: this.server,
-//       type: 'POST',
-//       data: JSON.stringify(message),
-//       contentType: 'application/json',
-//       success: function (data) {
-//         console.log('chatterbox: Message sent');
-//       },
-//       error: function (data) {
-//         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-//         console.error('chatterbox: Failed to send message', data);
-//       }
-//     });
-//   }
-  
-//   fetch() {
-//     $.ajax({
-//       url: this.server,
-//       type: 'GET',
-//       data: 'order=-createdAt',
-//       contentType: 'application/json',
-//       success: (data) => {
-//         $.each(data.results, (index, data) => {
-//           console.log(data);
-//           this.renderUsername(_.escape(data.username));
-//           this.renderMessage(_.escape(data.text));
-//         });
-        
-//       },
-//       error: function (data) {
-//         console.error('chatterbox: Failed to send message', data);
-//       }
-//     });
-//   }
-  
-//   clearMessages() {
-//     $('#chats').empty();
-//   }
-//   renderUsername(user) {
-//     $('#chats').append(`<div class="username"> ${user}
-//     </div>`);
-//   }
-//   renderMessage(message) { 
-//     $('#chats').append(`<div id="message">Message: ${message}
-//     </div>`);
-//   }
-
-//   renderRoom(room) {
-//     $('#roomSelect').append(`<div>${room}</div>`);
-//   }
-
-//   handleUsernameClick() {
-//     console.log('wtf');
-//   }
-
-//   handleSubmit(message) {
-//     this.send(message);
-//   }
-// }
-
-// let app = new App();
-// app.init();
-// app.fetch();
